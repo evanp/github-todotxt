c66fbb1f27b9513c1c08e5e41c52ed5669f1bff8
Evan Prodromou <evan@prodromou.name>
2019-04-11 12:51:58 -0400

    Use promises instead of async; closes #23

--- github-todotxt.js
+++ github-todotxt.js
@@ -2,7 +2,7 @@
 // github-todotxt.js
 // Get data from Github issues and format it for todo.txt
 //
-// Copyright 2016-2018 Evan Prodromou <evan@prodromou.name>
+// Copyright 2016-2019 Evan Prodromou <evan@prodromou.name>
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -22,7 +22,6 @@ const path = require('path')
 const _ = require('lodash')
 const yargs = require('yargs')
 const Octokit = require('@octokit/rest')
-const async = require('async')
 const split = require('split')
 
 const { argv } = yargs
@@ -44,22 +43,7 @@ const { argv } = yargs
   .alias('h', 'help')
 
 const projectCase = str => _.upperFirst(_.camelCase(str))
-
-const token = argv.t
-const filename = argv.f
-const quiet = (argv.q != null)
-
-const note = function (str) {
-  if (!quiet) {
-    return process.stdout.write(str)
-  }
-}
-
-const github = new Octokit({
-  auth: token
-})
-
-function markComplete (text, completed) {
+const markComplete = function(text, completed) {
   const m = text.match(/^\s*\(([A-Z])\)\s*/)
   if (m) {
     return `x ${completed} ${text.substr(m[0].length)} pri:${m[1]}`
@@ -68,10 +52,10 @@ function markComplete (text, completed) {
   }
 }
 
-async.parallel([
-  function (callback) {
+const getTodos = async function(filename) {
+  return new Promise((resolve, reject) => {
     const todos = []
-    return fs.createReadStream(filename)
+    fs.createReadStream(filename)
       .pipe(split())
       .on('data', (line) => {
         if (line.match(/\S/)) {
@@ -81,100 +65,129 @@ async.parallel([
           if (m) {
             [, todo.issue] = m
           }
-          return todos.push(todo)
+          todos.push(todo)
         }
-      }).on('error', err => callback(err, null)).on('end', () => callback(null, todos))
-  },
-  function (callback) {
-    if (!quiet) {
-      note('Getting issues...')
+      })
+      .on('error', reject)
+      .on('end', () => {
+        resolve(todos)
+      })
+  })
+}
+
+const writeTodos = async function(filename, todos) {
+  return new Promise((resolve, reject) => {
+    const ws = fs.createWriteStream(filename)
+      .on('error', reject)
+      .on('finish', resolve)
+
+    for (const todo of todos) {
+      ws.write(`${todo.text}\n`, 'utf-8')
     }
-    github.paginate('GET /issues')
-      .then(issues => {
-        callback(null, issues)
-      })
-      .catch(err => {
-        callback(err)
-      })
+
+    ws.end()
+  })
+}
+
+const rename = async function(oldname, newname) {
+  return new Promise((resolve, reject) => {
+    fs.rename(oldname, newname, (err) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve()
+      }
+    })
+  })
+}
+
+const main = async function(argv) {
+
+  const token = argv.t
+  const filename = argv.f
+  const quiet = (argv.q != null)
+
+  const note = function (str) {
+    if (!quiet) {
+      return process.stdout.write(str)
+    }
   }
-], (err, results) => {
-  if (err) {
-    return console.error(err)
-  } else {
-    let id, number, repo, todo
-    const [todos, issues] = Array.from(results)
-    if (!quiet) {
-      note(`${todos.length} lines in ${filename}\n`)
-      note(`${issues.length} issues on Github\n`)
+
+  const github = new Octokit({
+    auth: token
+  })
+
+  const issues = await github.paginate('GET /issues')
+  const todos = await getTodos(filename)
+
+  if (!quiet) {
+    note(`${todos.length} lines in ${filename}\n`)
+    note(`${issues.length} issues on Github\n`)
+  }
+
+  for (const issue of Array.from(issues)) {
+    repo = issue.repository.full_name;
+    ({ number } = issue)
+    id = `${repo}#${number}`
+    todo = _.find(todos, {issue: id})
+    if (todo != null) {
+      if (todo.text.match(/^x/)) {
+        if (issue.state === 'open') {
+          note('not closing issue')
+        }
+        // XXX: close the github issue
+      } else {
+        if (issue.state === 'closed') {
+          note(`Marking line for issue ${id} complete.\n`)
+          const completed = (issue.closed_at)
+            ? issue.closed_at.substr(0, 10)
+            : (new Date()).toISOString().substr(0, 10)
+          const {text} = todo
+          todo.text = markComplete(text, completed)
+        }
+      }
+    } else if (issue.state === 'open') {
+      note(`Adding line for issue ${id}.\n`)
+      const ts = issue.created_at.substr(0, 10)
+      const project = projectCase(repo.split('/')[1])
+      const { title } = issue
+      let line = `${ts} ${title} issue:${repo}#${number} +${project}`
+      if (issue.milestone != null) {
+        line += ` +${projectCase(issue.milestone.title)}`
+      }
+      todos.push({text: line})
     }
-    for (const issue of Array.from(issues)) {
+  }
+
+  // Todos with issues that aren't in the list should be marked as done.
+
+  for (todo of Array.from(todos)) {
+    if ((todo.issue == null)) {
+      continue
+    }
+    if (todo.text.match(/^x/)) {
+      continue
+    }
+    const issue = _.find(issues, (issue) => {
       repo = issue.repository.full_name;
       ({ number } = issue)
       id = `${repo}#${number}`
-      todo = _.find(todos, {issue: id})
-      if (todo != null) {
-        if (todo.text.match(/^x/)) {
-          if (issue.state === 'open') {
-            note('not closing issue')
-          }
-          // XXX: close the github issue
-        } else {
-          if (issue.state === 'closed') {
-            note(`Marking line for issue ${id} complete.\n`)
-            const completed = (issue.closed_at)
-              ? issue.closed_at.substr(0, 10)
-              : (new Date()).toISOString().substr(0, 10)
-            const {text} = todo
-            todo.text = markComplete(text, completed)
-          }
-        }
-      } else if (issue.state === 'open') {
-        note(`Adding line for issue ${id}.\n`)
-        const ts = issue.created_at.substr(0, 10)
-        const project = projectCase(repo.split('/')[1])
-        const { title } = issue
-        let line = `${ts} ${title} issue:${repo}#${number} +${project}`
-        if (issue.milestone != null) {
-          line += ` +${projectCase(issue.milestone.title)}`
-        }
-        todos.push({text: line})
-      }
-    }
-
-    // Todos with issues that aren't in the list should be marked as done.
-
-    for (todo of Array.from(todos)) {
-      if ((todo.issue == null)) {
-        continue
-      }
-      if (todo.text.match(/^x/)) {
-        continue
-      }
-      const issue = _.find(issues, (issue) => {
-        repo = issue.repository.full_name;
-        ({ number } = issue)
-        id = `${repo}#${number}`
-        return id === todo.issue
-      })
-      if ((issue == null)) {
-        note(`Issue ${todo.issue} not assigned to you; marking it done.\n`)
-        const completed = (new Date()).toISOString().substr(0, 10)
-        todo.text = markComplete(todo.text, completed)
-      }
-    }
-
-    const backup = `${filename}.bak`
-    return async.waterfall([
-      callback => fs.rename(filename, backup, callback),
-      function (callback) {
-        const texts = _.map(todos, 'text')
-        const data = `${texts.join('\n')}\n`
-        return fs.writeFile(filename, data, callback)
-      }
-    ], (err) => {
-      if (err) {
-        return console.error(err)
-      }
+      return id === todo.issue
     })
+    if ((issue == null)) {
+      note(`Issue ${todo.issue} not assigned to you; marking it done.\n`)
+      const completed = (new Date()).toISOString().substr(0, 10)
+      todo.text = markComplete(todo.text, completed)
+    }
   }
-})
+
+  const backup = `${filename}.bak`
+
+  note(`Backing up ${filename} to ${backup}...\n`)
+
+  await rename(filename, backup)
+
+  await writeTodos(filename, todos)
+}
+
+main(argv)
